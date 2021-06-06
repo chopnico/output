fee1b46db5e189b8dd435bba80d51c3cad45f9fa
chopnico <wes@audia.cloud>
2021-06-06 12:52:11 -0400

    FormatPrettyJson

--- json.go
+++ json.go
@@ -7,6 +7,22 @@ import (
 	"github.com/chopnico/structs"
 )
 
+func FormatPrettyJson(t *[]interface{}) string {
+	b := strings.Builder{}
+
+	for i := 0; i < len((*t)); i++ {
+		s := structs.New((*t)[i])
+		j, _ := json.MarshalIndent(s.Map(), "", "  ")
+
+		b.WriteString(string(j) + "\n")
+
+		if i != len((*t)) - 1 {
+			b.WriteString("\n")
+		}
+	}
+	return b.String()
+}
+
 func FormatJson(t *[]interface{}) string {
 	b := strings.Builder{}
 
