a549ae1d3e74d2470ce1a4661034cfe4b61c8017
chopnico <wes@audia.cloud>
2021-06-06 12:39:12 -0400

    adding FormatJson

---
+++ json.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strings"
+	"encoding/json"
+
+	"github.com/chopnico/structs"
+)
+
+func FormatJson(t *[]interface{}) string {
+	b := strings.Builder{}
+
+	for i := 0; i < len((*t)); i++ {
+		s := structs.New((*t)[i])
+		j, _ := json.Marshal(s.Map())
+
+		b.WriteString(string(j))
+
+		if i != len((*t)) - 1 {
+			b.WriteString("\n")
+		}
+	}
+	return b.String()
+}
