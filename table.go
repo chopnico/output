4b3e946de4e951fc3c1a0f0921849f033853c7b6
chopnico <wes@audia.cloud>
2021-07-04 06:54:42 -0400

    added GNU GPLv3 license

--- table.go
+++ table.go
@@ -1,3 +1,7 @@
+// Copyright 2021 chopnico All rights reserved.
+// Use of this source code is governed by a GNU GPLv3
+// license that can be found in the LICENSE file.
+
 package output
 
 import (
