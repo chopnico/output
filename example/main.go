f3cd3cfc633afedd085f4fbc1dce74193a841da6
chopnico <wes@audia.cloud>
2021-07-04 01:40:01 -0400

    single items were not properly padded; was sending properties instead of name of fields

--- example/main.go
+++ example/main.go
@@ -12,32 +12,32 @@ type Animal interface {
 }
 
 type Cow struct {
-	Name	string
-	Age			int
-	Alive		bool
-	Sound		string
-	Color		[]string
-	Weight  	float32
-	Intervals	[]int
-	Toys		*[]Toy
-	CreatedOn	time.Time
+	Name      string
+	Age       int
+	Alive     bool
+	Sound     string
+	Color     []string
+	Weight    float32
+	Intervals []int
+	Toys      *[]Toy
+	CreatedOn time.Time
 }
 
 type Dog struct {
-	Name		string
-	Age			int
-	Alive		bool
-	Sound		string
-	Color		[]string
-	Weight		float32
-	Intervals	[]int
-	Toys		*[]Toy
-	CreatedOn	time.Time
+	Name      string
+	Age       int
+	Alive     bool
+	Sound     string
+	Color     []string
+	Weight    float32
+	Intervals []int
+	Toys      *[]Toy
+	CreatedOn time.Time
 }
 
 type Toy struct {
 	Name string
-	Age int
+	Age  int
 	Best [3]int
 }
 
@@ -55,22 +55,22 @@ func Speak(a Animal) {
 
 func main() {
 	cow := Cow{
-		Name: "Will",
-		Age: 3,
-		Alive: true,
-		Sound: "moooo",
-		Color: []string{"white", "black"},
-		Weight: 3299.5,
-		Intervals: []int{1,20,30,100},
+		Name:      "Will",
+		Age:       3,
+		Alive:     true,
+		Sound:     "moooo",
+		Color:     []string{"white", "black"},
+		Weight:    3299.5,
+		Intervals: []int{1, 20, 30, 100},
 		Toys: &[]Toy{
 			{
 				Name: "brush",
-				Age: 3,
+				Age:  3,
 				Best: [3]int{1, 3, 5},
 			},
 			{
 				Name: "ball",
-				Age: 4,
+				Age:  4,
 				Best: [3]int{1, 3, 5},
 			},
 		},
@@ -78,23 +78,22 @@ func main() {
 	}
 
 	dog := Dog{
-		Name: "Rain",
-		Age: 2,
-		Alive: true,
-		Sound: "bark, bark",
-		Color: []string{"brown", "black", "white"},
-		Weight: 34.7,
-		Intervals: []int{1,20,10,34},
+		Name:      "Rain",
+		Age:       2,
+		Alive:     true,
+		Sound:     "bark, bark",
+		Color:     []string{"brown", "black", "white"},
+		Weight:    34.7,
+		Intervals: []int{1, 20, 10, 34},
 		Toys: &[]Toy{
 			{
 				Name: "rope",
-				Age: 8,
+				Age:  8,
 				Best: [3]int{1, 3, 5},
-
 			},
 			{
 				Name: "ball",
-				Age: 10,
+				Age:  10,
 				Best: [3]int{1, 3, 5},
 			},
 		},
@@ -108,6 +107,7 @@ func main() {
 	properties := []string{"Name", "Age", "Weight", "Toys", "Color", "CreatedOn"}
 
 	fmt.Printf("%s\n", output.FormatItemsAsList(animals, properties))
+	fmt.Printf("%s\n", output.FormatItemAsList(dog, nil))
 	fmt.Printf("%s\n", output.FormatItemsAsJson(animals))
 	fmt.Printf("%s", output.FormatItemsAsPrettyJson(animals))
 }
